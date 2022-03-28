cef8bc6cb1104167c431b91a203b30c99296cdae
s22763 <s22763@pjwstk.edu.pl>
2022-03-28 10:18:07 +0200

    student controller added

---
+++ cw3/WebApplication/Controllers/StudentsController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication.Controllers
+{
+    [ApiController]
+    [Route("api/students")]
+    public class StudentsController : ControllerBase
+    {
+        [HttpGet]
+        public string GetStudent()
+        {
+            return "Kowalski, Malewski, Andrzejewski";
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetStudent(int id)
+        {
+            if (id == 1)
+            {
+                return Ok("Kowalski");
+            }
+            else if (id == 2) {
+                return Ok("Malejeski");
+            }
+
+            return NotFound("Nie znaleziono studenta");
+        }
+    }
+}
